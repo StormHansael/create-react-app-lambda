3d5cab944cbc9e09ff31555a9fd61f0902635200
Storm Hansæl <stormoh@stud.ntnu.no>
2023-03-13 11:08:56 +0100

    Sup

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="Test" alt="This is meme" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
